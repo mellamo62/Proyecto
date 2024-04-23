561d62fb94716eb144599836923df6e0ef2fab18
mellamo62 <156244130+mellamo62@users.noreply.github.com>
2024-04-23 20:19:50 +0200

    rutas arregladas, pagina principal creada junto a cabecera.

--- Proyecto_integrado/src/main/frontend/src/app/app.component.ts
+++ Proyecto_integrado/src/main/frontend/src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Proyecto_integrado/src/main/frontend/src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    NgOptimizedImage,
+    RouterLink
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
