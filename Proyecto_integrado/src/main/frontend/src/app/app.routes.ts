8481a26b88556cd8b251afc308387fc061937bae
mellamo62 <156244130+mellamo62@users.noreply.github.com>
2024-04-25 18:26:54 +0200

    bara de navegacion creada. pagina prinicial creada

--- Proyecto_integrado/src/main/frontend/src/app/app.routes.ts
+++ Proyecto_integrado/src/main/frontend/src/app/app.routes.ts
@@ -1,8 +1,14 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {HomeComponent} from "./home/home.component";
+import {PrincipalComponent} from "./principal/principal.component";
 
 export const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: '', component: HomeComponent}
+  {
+    path: '', component: HomeComponent,
+    children: [
+      {path: 'login', component: LoginComponent},
+      {path: 'principal', component: PrincipalComponent}
+    ]
+  }
 ];
--- Proyecto_integrado/src/main/frontend/src/app/home/home.component.ts
+++ Proyecto_integrado/src/main/frontend/src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
-import {RouterLink} from "@angular/router";
+import {RouterLink, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    RouterLink
+    RouterLink,
+    RouterOutlet
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
